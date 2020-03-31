d2a919a731fd8066e7922917732402bd61870358
Ahmad Rifa'i <arifai209@gmail.com>
2020-04-01 00:20:31 +0700

    Change emoji in about

--- src/commands/system_info/about.js
+++ src/commands/system_info/about.js
@@ -1,7 +1,7 @@
 const about = async (ctx) => {
     const { BOT_USERNAME, VERSION } = process.env;
 
-    const msg = `*### Tentang ###*\n🤖 Bot name: @${BOT_USERNAME}\n🎉 Version: ${VERSION}\n🧑 Powered by: @weneedenter`;
+    const msg = `*### Tentang ###*\n🤖 Bot name: @${BOT_USERNAME}\n🎉 Version: ${VERSION}\n👨‍💻 Powered by: @weneedenter`;
 
     return await ctx.replyWithMarkdown(msg);
 }
