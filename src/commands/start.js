26f18b744332488a48a1abdeac1087d56518cdc4
Ahmad Rifa'i <arifai209@gmail.com>
2020-05-05 02:00:31 +0700

    Implement required telegram username

--- src/commands/start.js
+++ src/commands/start.js
@@ -3,10 +3,16 @@ const config = require('./config');
 
 const start = async (ctx) => {
     const firstName = ctx.from.first_name;
-    const msg = `Hai ${firstName}, selamat datang di *Crypto Merchant* 😊`;
+    const username = ctx.from.username;
     const keyboard = Markup.keyboard(config.mainMenu).resize().extra();
 
-    return await ctx.replyWithMarkdown(msg, keyboard);
+    const msgWithUsername = `Hai ${firstName}, selamat datang di *Crypto Merchant* 😊`;
+    const msgWithoutUsername = `Hai ${firstName}, selamat datang di *Crypto Merchant*. Untuk menggunakan Crypto Merchant silahkan menambahkan username Telegram terlebih dahulu ya, jika sudah silahkan gunakan perintah /start kembali.`;
+
+    const msg = username == null ? msgWithoutUsername : msgWithUsername;
+    const kybd = username == null ? null : keyboard;
+
+    return await ctx.replyWithMarkdown(msg, kybd);
 }
 
 module.exports = { start };
